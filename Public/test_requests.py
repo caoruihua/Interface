940de01ff12ce24ac4306efe7e7b9d15672c4aad
apple <499800885@qq.com>
2018-10-29 16:18:12 +0800

    常规修改

--- Public/test_requests.py
+++ Public/test_requests.py
@@ -1,43 +1,50 @@
 # -*- coding: utf-8 -*-
 # @Author  : laocao
-import requests,json
-from Public.log import LOG,logger
+import requests, json
+from Public.log import LOG, logger
+
+
 @logger('requests封装')
 class requ():
     def __init__(self):
-        self.headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:51.0) Gecko/20100101 Firefox/51.0",
-                        "Authorization": "Bearer cf8d86e3-4ae9-4800-a937-714781106643"}
-    def get(self, url,params):#get消息
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:51.0) Gecko/20100101 Firefox/51.0",
+            }
+
+    def get(self, url, params):  # get消息
         try:
-            r = requests.get(url, params=params,headers=self.headers)
+            r = requests.get(url, params=params, headers=self.headers)
             r.encoding = 'UTF-8'
             json_response = json.loads(r.text)
-            return {'code':0,'result':json_response}
+            return {'code': 0, 'result': json_response}
         except Exception as e:
-            LOG.info('get请求出错，出错原因:%s'%e)
-            return {'code': 1, 'result': 'get请求出错，出错原因:%s'%e}
-    def post(self, url, params):#post消息
-        data = json.dumps(params)
+            LOG.info('get请求出错，出错原因:%s' % e)
+            return {'code': 1, 'result': 'get请求出错，出错原因:%s' % e}
+
+    def post(self, url, params):  # post消息
         try:
-            r =requests.post(url,params=data,headers=self.headers)
-            json_response = json.loads(r.text)
+            P = requests.post(url, params=params, headers=self.headers)
+            P.encoding = 'UTF-8'
+            json_response = json.loads(P.text)
             return {'code': 0, 'result': json_response}
         except Exception as e:
             LOG.info('post请求出错，出错原因:%s' % e)
             return {'code': 1, 'result': 'post请求出错，出错原因:%s' % e}
-    def delfile(self,url,params):#删除的请求
+
+    def delfile(self, url, params):  # 删除的请求
         try:
-            del_word=requests.delete(url,params=params,headers=self.headers)
-            json_response=json.loads(del_word.text)
+            del_word = requests.delete(url, params=params, headers=self.headers)
+            json_response = json.loads(del_word.text)
             return {'code': 0, 'result': json_response}
         except Exception as e:
             LOG.info('del请求出错，出错原因:%s' % e)
             return {'code': 1, 'result': 'del请求出错，出错原因:%s' % e}
-    def putfile(self,url,params):#put请求
+
+    def putfile(self, url, params):  # put请求
         try:
-            data=json.dumps(params)
-            me=requests.put(url,data)
-            json_response=json.loads(me.text)
+            data = json.dumps(params)
+            me = requests.put(url, data)
+            json_response = json.loads(me.text)
             return {'code': 0, 'result': json_response}
         except Exception as e:
             LOG.info('put请求出错，出错原因:%s' % e)
